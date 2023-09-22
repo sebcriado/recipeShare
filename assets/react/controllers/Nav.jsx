2487dee44d56b48d897f59312b96e2dcd751d3d2
Criado Sébastien <dev@letgy.com>
2023-09-22 19:13:13 +0200

    feat: :sparkles: Add navbar with logo

---
+++ assets/react/controllers/Nav.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function (){
+    return (
+    <div className="w-full fixed top-0 left-0">
+        <div className="flex bg-blue-500 py-5">
+            <div className="font-bold text-2xl cursor-pointer flex items-center font-[Poppins] text-white">
+                <span className="text-3xl mr-1 pt-2"><ion-icon name="share-social-outline"></ion-icon></span>
+                RecipeShare
+            </div>
+        </div>
+    </div>
+    );
+}
